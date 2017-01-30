eb7b1e55bd0f7b2fc66ee97e96a154ffb556dba7
bubbadaboss77 <rfeldman@bates.edu>
2017-01-30 23:32:43 +0000

    updated heroku psql

--- text/config.py
+++ text/config.py
@@ -1,6 +1,6 @@
 import os
 
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/textbook"
+    SQLALCHEMY_DATABASE_URI = "postgres://pfhuoxshwdihhf:981ecaa3cb0ac95c658610b7122a8a08e80a64d1bb7a1bac0f9b3f739166b7e3@ec2-54-235-247-224.compute-1.amazonaws.com:5432/ddq4e8054n9hok"
     DEBUG = True
     SECRET_KEY = os.environ.get("TEXTBOOK_SECRET_KEY", os.urandom(12))
